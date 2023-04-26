0ee595bda858ed16fd54fa233c16b884c23e92de
puerarongo <akhardin96@gmail.com>
2023-04-26 20:31:57 +0300

    add header

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import styles from "./Navigation.module.css";
 import { NavLink } from "react-router-dom";
+import svgPath from "../../services/svgPath";
 
 const Navigation: React.FC = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logo__container}>
-        <svg className={styles.svg__logo}>
-          <use href="#"></use>
-        </svg>
+      <nav className={styles.nav}>
+        <div className={styles.logo__container}>
+          <svg className={styles.svg__logo}>
+            <use href={svgPath.pizza + "#pizza"}></use>
+          </svg>
 
-        <NavLink className={styles.link} to="/">
-          Pizza
-        </NavLink>
-      </div>
-      <div className={styles.cart__container}>
-        <NavLink className={styles.link} to="/cart">
-          Cart
-        </NavLink>
-      </div>
+          <NavLink className={styles.link} to="/">
+            Pizza
+          </NavLink>
+        </div>
+        <div className={styles.cart__container}>
+          <NavLink className={styles.link} to="/cart">
+            <svg className={styles.svg}>
+              <use href={svgPath.basket + "#basket"}></use>
+            </svg>
+          </NavLink>
+        </div>
+      </nav>
     </header>
   );
 };
---
+++ src/services/svgPath.ts
@@ -0,0 +1,9 @@
+import basket from "../img/basket.svg";
+import pizza from "../img/pizza.svg";
+
+const svgPath = {
+  basket,
+  pizza,
+};
+
+export default svgPath;
